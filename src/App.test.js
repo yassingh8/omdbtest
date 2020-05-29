232a1051444e77c0f9b0f5c6c2dab3dff61c05b5
Yashwardhan Singh <yashwardhan.singh@publicissapient.com>
2020-05-29 16:19:38 +0530

    jest enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { shallow } from 'enzyme';
+import MoviesList from './components/MovieList';
+
+describe('<App />', () => {
+  test('renders without crashing', () => {
+    // shallow(<App/>);
+    const wrapper = shallow(<App/>);
+    const movieslist = <MoviesList/>;
+    expect(wrapper.contains(movieslist)).toEqual(true);
+  });
 });
