bd66611757a845d85635349d6184f8aa5f5a21a3
Yashwardhan Singh <yashwardhan.singh@publicissapient.com>
2020-05-29 18:11:07 +0530

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,22 @@ import App from './App';
 
 import { shallow } from 'enzyme';
 import MoviesList from './components/MovieList';
+import toJson from 'enzyme-to-json';
+import configureStore from 'redux-mock-store'; // Smart components
+
+const mockStore = configureStore();
+
+const initialState = {};
+
+const store = mockStore(initialState);
 
 describe('<App />', () => {
   test('renders without crashing', () => {
     // shallow(<App/>);
-    const wrapper = shallow(<App/>);
-    const movieslist = <MoviesList/>;
-    expect(wrapper.contains(movieslist)).toEqual(true);
+    const wrapper = shallow(<App store={store}/>);
+    // const movieslist = <MoviesList/>;
+    // expect(wrapper.contains(movieslist)).toEqual(true);
+    const componentss = wrapper.dive();
+    expect(toJson(componentss)).toMatchSnapshot();
   });
 });
--- src/components/MovieList.test.js
+++ src/components/MovieList.test.js
@@ -8,22 +8,51 @@ import Movie from './Movie';
 import App from '../App';
 import configureStore from 'redux-mock-store'; // Smart components
 import toJson from 'enzyme-to-json';
+import favourite from '../reducers/favourite';
+import {SEARCH_MOVIE, FETCH_MOVIES, SHOW_LIST} from '../config/Strings';
 
 const mockStore = configureStore();
 
 const initialState = {
-    movieList: [],
-    myList: [],
-    searchTerm: '',
-    showMy: false
+    favourite: {
+        movieList: [],
+        myList: [],
+        searchTerm: '',
+        showMy: false
+    }
 };
 
 const store = mockStore(initialState);
 
 describe('<MovieList />', () => {
     test('renders without crash', () => {
-        const wrapper = shallow(<MovieList store={store}/>).dive();
-        // const movie = wrapper.dive();
-        expect(toJson(wrapper)).toMatchSnapshot();
+        const wrapper = shallow(<MovieList store={store}/>);
+        const movies = wrapper.dive();
+        expect(toJson(movies)).toMatchSnapshot();
     });
 });
+
+describe('INITIAL_STATE', () => {
+    test('is correct', () => {
+      const action = { type: 'SEARCH_MOVIE'};
+  
+      expect(favourite(undefined, action)).toMatchSnapshot();
+    });
+  })
+
+describe('SHOW_LIST', () => {
+    test('is correct', () => {
+      const action = { type: 'SHOW_LIST', payload: []};
+  
+      expect(favourite(undefined, action)).toMatchSnapshot();
+    });
+  })
+
+// describe('FETCH_MOVIES', () => {
+//     test('is correct', () => {
+//       const action = { type: 'FETCH_MOVIES'};
+  
+//       expect(favourite(undefined, action)).toMatchSnapshot();
+//     });
+//   })
+ 
