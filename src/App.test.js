8e3a1c82ee66476aa12486c2b8873591ea2bae12
Yashwardhan Singh <yashwardhan.singh@publicissapient.com>
2020-06-01 17:58:03 +0530

    actions and reducer jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import App from './App';
 
-import { shallow } from 'enzyme';
-import MoviesList from './components/MovieList';
+import { shallow, mount } from 'enzyme';
+import MovieList from './components/MovieList';
+import toJson from 'enzyme-to-json';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+const mockStore = configureStore();
+
+const initialState = {
+  favourite: {
+    movieList: [],
+    myList: [],
+    searchTerm: '',
+    showMy: false
+  }
+};
+
+const store = mockStore(initialState);
 
 describe('<App />', () => {
   test('renders without crashing', () => {
     // shallow(<App/>);
-    const wrapper = shallow(<App/>);
-    const movieslist = <MoviesList/>;
-    expect(wrapper.contains(movieslist)).toEqual(true);
+    const wrapper = mount(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    // const movielist = <MovieList/>;
+    const components = wrapper//.dive();
+    expect(wrapper.find('App').length).toEqual(1);
   });
 });
--- src/components/MovieList.test.js
+++ src/components/MovieList.test.js
@@ -2,28 +2,34 @@ import React from 'react';
 // import { render } from '@testing-library/react';
 // import ReactDOM from 'react-dom';
 
-import { shallow } from 'enzyme';
+import { shallow, mount, ReactWrapper } from 'enzyme';
 import MovieList from './MovieList';
 import Movie from './Movie';
 import App from '../App';
 import configureStore from 'redux-mock-store'; // Smart components
 import toJson from 'enzyme-to-json';
+import { Provider } from 'react-redux';
 
 const mockStore = configureStore();
 
 const initialState = {
-    movieList: [],
-    myList: [],
-    searchTerm: '',
-    showMy: false
+    favourite: {
+        movieList: [],
+        myList: [],
+        searchTerm: '',
+        showMy: false
+      }
 };
 
 const store = mockStore(initialState);
 
 describe('<MovieList />', () => {
     test('renders without crash', () => {
-        const wrapper = shallow(<MovieList store={store}/>).dive();
-        // const movie = wrapper.dive();
-        expect(toJson(wrapper)).toMatchSnapshot();
+        const wrapper = mount(
+            <Provider store={store}>
+                <MovieList store={store}/>
+            </Provider>
+        );
+        expect(wrapper.find('MovieList').length).toEqual(1);
     });
 });
